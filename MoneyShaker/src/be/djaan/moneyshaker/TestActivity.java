affb14c94d571bb837ffb9c2b3cac2fd5e97af60
Django <django.steemans@gmail.com>
2013-06-06 20:02:38 +0200

    test2

---
+++ MoneyShaker/src/be/djaan/moneyshaker/TestActivity.java
@@ -0,0 +1,13 @@
+package be.djaan.moneyshaker;
+
+public class TestActivity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
